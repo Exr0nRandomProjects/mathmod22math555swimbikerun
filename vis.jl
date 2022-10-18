87aa4e298077b05aff9519ed27f20caf8fa33d0d
Exr0n <mail@exr0n.com>
2022-10-17 19:57:49 -0700

    u can just broadcast the pair operator?? link below
    https://discourse.julialang.org/t/transform-multiple-columns-of-a-dataframe-using-the-same-function/61550

--- vis.jl
+++ vis.jl
@@ -1,6 +1,9 @@
+using Chain
+
 using DataFrames, CSV
 using Plots
 using Dates
+using Statistics
 
 df = CSV.File("cleaned_dataset.csv") |> DataFrame;
 
@@ -8,16 +11,28 @@ df = CSV.File("cleaned_dataset.csv") |> DataFrame;
 
 keys = ["SWIM" "BIKE" "RUN"]
 
-data, keys = keys .|> (k -> [df[!, k] .|> Dates.value .|> (x -> x/1e9), lowercase(k)]) |> (x -> zip(x...)) |> collect
-# print(typeof(data), typeof(keys), typeof(collect(keys)), typeof([keys...]), typeof(reshape(collect(keys), :, 1)))
-# print([permutedims(collect(keys))] .|> typeof)
+# data, keys = keys .|> (k -> [df[!, k] .|> Dates.value .|> (x -> x/1e9), lowercase(k)]) |> (x -> zip(x...)) |> collect
+# histogram([data...], xlabel="time", ylabel="number of contestants", label=permutedims(collect(keys)))   # have to do this cursed permutedims thing because plots.jl treats first dim as things to cycle thru in the current series, and second dim as a diff series. 
 
-histogram([data...], xlabel="time", ylabel="number of contestants", label=permutedims(collect(keys)))   # have to do this cursed permutedims thing because plots.jl treats first dim as things to cycle thru in the current series, and second dim as a diff series. 
+using Chain
 
+res = @chain df begin
+    dropmissing
+    groupby(_, :CATEGORY)
+    # transform(_, :SWIM => (x -> Dates.value(x)/1e9), renamecols=false)
+    transform(_, [:SWIM, :T1, :BIKE, :T2, :RUN] .=> (x -> Dates.value.(x)/1e9), renamecols=false)
+    # transform(_, [:SWIM, :T1, :BIKE, :T2, :RUN] .=> (x -> println(x)), renamecols=false)
+end
 
-
-# p = histogram()
-# keys .|> (k -> histogram!(p, df[!, k], xlabel="time", ylabel="# contestants", label=k))
-# p
-# # print(length(data))
+# series, labels = 
 # 
+# gd = groupby(df, :CATEGORY)
+# gd |> (gd -> combine(gd, :SWIM => mean))
+
+# print(gd[1]["SWIM"])
+# gd .|> println
+# data, labels = gd .|> (g -> [g[!, "SWIM"], g[!, "CATEGORY"]]) |> (x -> zip(x...))
+# histogram([gd...], xlabel="time", ylabel="# ppl", label=permutedims(collect(labels)))
+
+
+
