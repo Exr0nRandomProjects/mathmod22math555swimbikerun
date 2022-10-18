a92b23770f7a14ce8cb266f6daadf4837dcaf323
Exr0n <mail@exr0n.com>
2022-10-18 00:40:52 -0700

    partial cum_sums over the course of the race

--- vis.jl
+++ vis.jl
@@ -16,27 +16,49 @@ df = CSV.File("cleaned_dataset.csv") |> DataFrame;
 const TIME_COMPONENTS = ["SWIM", "T1", "BIKE", "T2", "RUN"]
 
 grouped_to_2d(gd) = @chain gd select(TIME_COMPONENTS)
+kv_namedtuple(ks, vs) = (; zip(Symbol.(ks), vs)... )
 
 df = @chain df begin
     dropmissing
-    transform(_, TIME_COMPONENTS .=> ByRow(x -> Dates.value(x)/1e9), renamecols=false)
+    transform(TIME_COMPONENTS .=> ByRow(x -> Dates.value(x)/1e9), renamecols=false)
     select(cat(TIME_COMPONENTS, ["CATEGORY", "GENDER"]; dims=1))
 end
 
-# plot each sport seprately, overlay by gender
-# println(typeof(df))
-# plot(histogram(group[:T]))
-gd = @chain df groupby(:GENDER)
-println(first(gd))
-data_by_gender_by_sport = Dict(
-    key => Dict(
-        group[1, :GENDER] => group[!, key] |> collect
-        for group in gd
-    ) for key in TIME_COMPONENTS
-)   # cringe because "broadcasting over GroupedDataFrames is reserved"
-compare_gender_forall_sports = [
-    histogram(d_by_g|>values|>collect, label=d_by_g|>keys|>collect|>permutedims,
-              opacity=0.7, title=sport, xrotation=45)
-    for (sport, d_by_g) in data_by_gender_by_sport ]
-plot(compare_gender_forall_sports...)
+# # function compare_gender_forall_sports()
+# # plot each sport seprately, overlay by gender
+# gd = @chain df groupby(:GENDER)
+# println(first(gd))
+# data_by_gender_by_sport = Dict(
+#     key => Dict(
+#         group[1, :GENDER] => group[!, key] |> collect
+#         for group in gd
+#     ) for key in TIME_COMPONENTS
+# )   # cringe because "broadcasting over GroupedDataFrames is reserved"
+# charts = [
+#     histogram(d_by_g|>values|>collect, label=d_by_g|>keys|>collect|>permutedims,
+#               opacity=0.7, title=sport, xrotation=45)
+#     for (sport, d_by_g) in data_by_gender_by_sport ]
+# plot(charts...)
+# # end
+
+# function compare_sports()
+# d_by_s = Dict( k => df[!, k] for k in TIME_COMPONENTS )
+# histogram(d_by_s|>values|>collect, labels=(@chain d_by_s keys collect permutedims),
+#           linewidth=0, xlabel="time (s)", ylabel="# contestants", opacity=0.8)
+# end
+
+
+# cum_df = @chain df begin
+#     transform(_, TIME_COMPONENTS => ByRow((r...) -> Dict(
+#         k => v
+#     for (k, v) in zip(
+#         (@chain TIME_COMPONENTS string.("cum_", _)),
+#         r |> cumsum
+#     ))) => AsTable)
+# end
+cum_df = @chain df begin
+    transform(_, TIME_COMPONENTS => ByRow(
+        (r...) -> kv_namedtuple(  (@chain TIME_COMPONENTS string.("cum_", _)), r |> cumsum  )
+    ) => AsTable)
+end
 
